f4312b3785b2ced8b6c268b06de1d519e709318d
Ivor King <ivorking@gmail.com>
2018-04-03 15:36:23 +1000

    more changes

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,10 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
+    has_secure_password
+    devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
     has_and_belongs_to_many :pols
     belongs_to :state
+    validates :email, :presence => true, :uniqueness => true
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,13 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   get '/blogs' => 'pages#blogs'
-  
+
+  root :to => 'pages#home'
+
+  resources :users, :only => [:new, :create]
+
+  get '/login' => 'session#new'        # Sign in form
+  post '/login' => 'session#create'    # Sign in action
+  delete '/login' => 'session#destroy' # Sign out
+
 end
