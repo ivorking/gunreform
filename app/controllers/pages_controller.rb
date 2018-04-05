9788b013098ae838b54da5478f09215836604791
Ivor King <ivorking@gmail.com>
2018-04-05 16:33:59 +1000

    updated pages

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -50,10 +50,12 @@ class PagesController < ApplicationController
     @pol = Pol.find_by(name_first: @name1, name_last: @name2)
     if @pol != nil
       @user.pols << @pol
-      @outputmsg = "Match found in the site database!"
+      @outputmsg = "Match found in the site politician database!"
     else
-      @outputmsg = "No match found in the site database."
+      @outputmsg = "No match found in the site politician database."
     end 
+
+    # Site currently uses scrape from opensecrets.org for funding data, and is limited to the US House of Reps. This will be expanded once a suitable API has been chosen.
 
     require 'open-uri'
     @base_url = 'https://www.politico.com/interactives/2017/gun-lobbying-spending-in-america-congress/'
@@ -62,7 +64,7 @@ class PagesController < ApplicationController
     @funding_list = @raw_data.css('td:nth(2)').map &:text
     @candidate_list.each_with_index do | value, index |
       if @candidate_list[index].upcase == @name_selected
-        @locatormsg = "Match found to gun lobby finance website - US House of Representatives members. Candidate received US #{@funding_list[index]} over the course of their career."
+        @locatormsg = "Match found to gun lobby finance website - US House of Representatives members. Candidate received US #{@funding_list[index]} over the course of their career (as at Oct 2017)."
       end
     end
   end
